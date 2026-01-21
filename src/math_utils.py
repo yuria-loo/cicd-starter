88aa7a541bb468e7f3f94500b9aaf3e794ca0d15
Yuria Loo <yuria.loo@comcast.net>
2026-01-20 19:48:01 -0800

    fix the func

--- src/math_utils.py
+++ src/math_utils.py
@@ -3,4 +3,4 @@ def add(a: int, b: int) -> int:
     Return the sum of two integers.
     Students will deliberately break this, then fix it to make CI pass.
     """
-    return a + b + 1
+    return a + b
